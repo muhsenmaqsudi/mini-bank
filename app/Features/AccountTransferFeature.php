05276e4ce9cf76409c85e69c18f3e0a15f9c775b
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2024-02-10 22:53:14 +0330

    Added cache locks for locking account transaction before doing the transfer

--- app/Features/AccountTransferFeature.php
+++ app/Features/AccountTransferFeature.php
@@ -8,6 +8,7 @@ use App\Models\Card;
 use App\Models\Transaction;
 use App\ValueObjects\TransactionType;
 use App\ValueObjects\TransferType;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
@@ -15,52 +16,54 @@ class AccountTransferFeature
 {
     public function handle(AccountTransferDTO $transferable): Transaction
     {
-        /** @var Transaction $transaction */
-        $transaction = DB::transaction(function () use ($transferable) {
-            /** @var Card $senderCard */
-            $senderCard = Card::query()->where('card_no', $transferable->senderCard)->firstOrFail();
-            $transferable->setSenderAccount($senderCard->account_id);
+        $transaction = Cache::lock($transferable->senderCard, 10)->get(function () use ($transferable) {
+            /** @var Transaction $transaction */
+            return DB::transaction(function () use ($transferable) {
+                /** @var Card $senderCard */
+                $senderCard = Card::query()->where('card_no', $transferable->senderCard)->firstOrFail();
+                $transferable->setSenderAccount($senderCard->account_id);
 
-            /** @var Card $receivingCard */
-            $receivingCard = Card::query()->where('card_no', $transferable->receivingCard)->firstOrFail();
-            $transferable->setReceivingAccount($receivingCard->account_id);
+                /** @var Card $receivingCard */
+                $receivingCard = Card::query()->where('card_no', $transferable->receivingCard)->firstOrFail();
+                $transferable->setReceivingAccount($receivingCard->account_id);
 
-            if ($transferable->amount <= $senderCard->account->balance) {
-                /** @var Transaction $txn */
-                $txn = Transaction::query()->create([
-                    'track_id' => Str::uuid()->toString(),
-                    'card_id' => $senderCard->id,
-                    'type' => TransactionType::TRANSFER->value,
-                    'amount' => $transferable->amount,
-                ]);
+                if ($transferable->amount <= $senderCard->account->balance) {
+                    /** @var Transaction $txn */
+                    $txn = Transaction::query()->create([
+                        'track_id' => Str::uuid()->toString(),
+                        'card_id' => $senderCard->id,
+                        'type' => TransactionType::TRANSFER->value,
+                        'amount' => $transferable->amount,
+                    ]);
 
-                $txn->transfers()->create([
-                    'sender_id' => $transferable->senderCard,
-                    'receiving_id' => $transferable->receivingCard,
-                    'amount' => $transferable->amount,
-                    'type' => TransferType::WITHDRAW->value,
-                ]);
-                $txn->transfers()->create([
-                    'sender_id' => $transferable->receivingCard,
-                    'receiving_id' => $transferable->senderCard,
-                    'amount' => $transferable->amount,
-                    'type' => TransferType::DEPOSIT->value,
-                ]);
+                    $txn->transfers()->create([
+                        'sender_id' => $transferable->senderCard,
+                        'receiving_id' => $transferable->receivingCard,
+                        'amount' => $transferable->amount,
+                        'type' => TransferType::WITHDRAW->value,
+                    ]);
+                    $txn->transfers()->create([
+                        'sender_id' => $transferable->receivingCard,
+                        'receiving_id' => $transferable->senderCard,
+                        'amount' => $transferable->amount,
+                        'type' => TransferType::DEPOSIT->value,
+                    ]);
 
-                $txn->wage()->create([
-                    'fee' => 5000
-                ]);
+                    $txn->wage()->create([
+                        'fee' => 5000
+                    ]);
 
-                $senderCard->account->update([
-                    'balance' => $senderCard->account->balance - $transferable->amount - 5000
-                ]);
+                    $senderCard->account->update([
+                        'balance' => $senderCard->account->balance - $transferable->amount - 5000
+                    ]);
 
-                $receivingCard->account->update([
-                    'balance' => $receivingCard->account->balance + $transferable->amount
-                ]);
+                    $receivingCard->account->update([
+                        'balance' => $receivingCard->account->balance + $transferable->amount
+                    ]);
 
-                return $txn;
-            };
+                    return $txn;
+                };
+            });
         });
 
         SuccessfulTransferOccurred::dispatch($transferable);
